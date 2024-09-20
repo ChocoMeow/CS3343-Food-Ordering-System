37c6ea65a275ff5c8f944f97482563da1c210f8a
Choco <94597336+ChocoMeow@users.noreply.github.com>
2024-09-20 14:57:20 +0800

    Added config class

---
+++ food-ordering-system/src/main/java/com/fos/main/Config.java
@@ -0,0 +1,59 @@
+package com.fos.main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Config {
+    public static final String configPath = "./food-ordering-system/src/main/resources/Configuration.json";
+
+    @SerializedName("CHEF_NUM")
+    private int chefNum;
+
+    @SerializedName("BARTENDER_NUM")
+    private double bartenderNum;
+
+    @SerializedName("BATCH_JOB_PER_TIME")
+    private double batchJobPerTime;
+
+    @SerializedName("ORDER_DEQUEUE_THRESHOLD")
+    private double orderDequeueThreshold;
+
+    @SerializedName("ORDER_INTERVAL_CONFIG")
+    private double orderIntervalConfig;
+
+    public static Config loadConfig() {
+        Gson gson = new Gson();
+        Config config = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(configPath))) {
+            config = gson.fromJson(br, Config.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return config;
+    }
+
+    public void saveConfig() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create(); // For pretty printing
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(configPath))) {
+            gson.toJson(this, bw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- food-ordering-system/src/main/java/com/fos/main/Main.java
+++ food-ordering-system/src/main/java/com/fos/main/Main.java
@@ -4,26 +4,38 @@ import com.fos.factories.CommandFactory;
 import com.fos.invokers.CommandInvoker;
 import com.fos.commands.Command;
 
+import com.fos.Tasks.TaskPool;
+import com.fos.Tasks.Task;
+
+import com.fos.foods.Food;
+import com.fos.drinks.Drink;
+
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
+
+    public static Task foodQueueTask = TaskPool.getTask("main");
+    public static ArrayList<Food> foodQueue = new ArrayList<Food>();
+    public static ArrayList<Drink> drinkQueue = new ArrayList<Drink>();
+
     public static void main(String[] args) {
         CommandFactory commandFactory = new CommandFactory();
         CommandInvoker invoker = new CommandInvoker();
         Scanner scanner = new Scanner(System.in);
 
         List<Command> commands = new ArrayList<>(commandFactory.getAllCommands());
+        
+        Config config = Config.loadConfig();
 
         while (true) {
             System.out.println("Menu:");
             for (int i = 0; i < commands.size(); i++) {
-                System.out.printf("%d. Execute %s%n", (i + 1), commands.get(i).getClass().getSimpleName());
+                System.out.printf("%d. %s%n", (i + 1), commands.get(i).getName());
             }
             System.out.println((commands.size() + 1) + ". Undo Last Command");
-            System.out.println((commands.size() + 2) + ". Redo Last Command");
-            System.out.println((commands.size() + 3) + ". Exit");
+            System.out.println((commands.size() + 2) + ". Exit");
             System.out.print("Enter your choice: ");
 
             int choice = scanner.nextInt();
@@ -32,12 +44,11 @@ public class Main {
                 Command command = commands.get(choice - 1);
                 invoker.executeCommand(command);
             } else if (choice == commands.size() + 1) {
-                invoker.undo();
-            } else if (choice == commands.size() + 2) {
                 invoker.redo();
-            } else if (choice == commands.size() + 3) {
+            } else if (choice == commands.size() + 2) {
                 System.out.println("Exiting...");
                 scanner.close();
+                TaskPool.stopAllTasks();
                 return;
             } else {
                 System.out.println("Invalid choice. Please try again.");
