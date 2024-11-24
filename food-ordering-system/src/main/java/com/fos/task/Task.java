2fbe0f43fa1df48f2c5a24e974aa918f20e50811
Choco <94597336+ChocoMeow@users.noreply.github.com>
2024-11-24 21:24:06 +0800

    Create Task.java

---
+++ food-ordering-system/src/main/java/com/fos/task/Task.java
@@ -0,0 +1,27 @@
+package com.fos.task;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public abstract class Task {
+    private final ScheduledExecutorService scheduler;
+    private int initialDelay;
+    private int period;
+
+    public Task(int initialDelay, int period) {
+        this.scheduler = Executors.newScheduledThreadPool(1);
+        this.initialDelay = initialDelay;
+        this.period = period;
+    }
+
+    public void start() {
+        this.scheduler.scheduleAtFixedRate(this::execute, this.initialDelay, period, TimeUnit.SECONDS);
+    }
+
+    public void stop() {
+        this.scheduler.shutdown();
+    }
+
+    public abstract void execute();
+}
